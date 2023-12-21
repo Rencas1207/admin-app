91d3eea2f570e644f21cc318c6068aea2895f3c3
Rencas <rencasdag.12@gmail.com>
2023-12-20 21:49:16 -0500

    mongodb + ts #2

--- backend/db/connect.ts
+++ backend/db/connect.ts
@@ -9,18 +9,6 @@ async function connectDB() {
   try {
     await mongoose.connect(process.env.MONGODB_URL);
     console.log('Conexion exitosa con mongodb');
-    //  const newUser = new UserModel({
-    //    firstname: 'Renzo',
-    //    lastname: 'Castilla',
-    //    email: 'rencasdag.12@gmail.com',
-    //    login_code: '123456',
-    //    roles: {
-    //      admin: true,
-    //      seller: true,
-    //    },
-    //  });
-    //  console.log({ newUser });
-    //  await newUser.save();
   } catch (error) {
     console.log('Hubo un error al conectarnos a la bd');
   }
